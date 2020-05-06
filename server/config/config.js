fa5c1f5751426d5d985f0f3a6698a2bf3c9084e8
Neil Tate <57351607+Neil-Tate@users.noreply.github.com>
2020-05-06 22:28:16 +0200

    commit to run server

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,6 @@
 //     RESTAPIport: '5000',
 // };
 module.exports = {
-    db: 'mongodb+srv://Robert:Professional>@cluster0-m5kjg.mongodb.net/test',
+    db: 'mongodb+srv://Robert:Professional@cluster0-m5kjg.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true',
     RESTAPIport: '5000',
 };
