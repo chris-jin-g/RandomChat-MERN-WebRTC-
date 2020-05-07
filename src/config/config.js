8227080b6e0141ee019850e40237f980f61f0ec6
Neil Tate <57351607+Neil-Tate@users.noreply.github.com>
2020-05-07 12:55:48 +0200

    second commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,5 @@
-export const RESTAPIUrl = 'http://192.168.100.118:5000';
-export const SOCKET_URI = 'http://localhost:5000';
+// export const RESTAPIUrl = 'http://192.168.100.118:5000';
+// export const SOCKET_URI = 'http://localhost:5000';
+
+export const RESTAPIUrl = 'https://anonymous-video-chat.herokuapp.com/';
+export const SOCKET_URI = 'https://anonymous-video-chat.herokuapp.com/';
